22be8c18cb26fe85b32fef73da90dd06cb60e487
Raghavendra Vaddi <34688769+vaddiraghava123@users.noreply.github.com>
2025-02-26 06:56:00 -0500

    Update ImageAnalytics_GoogleGemini.py

--- ImageAnalytics_GoogleGemini.py
+++ ImageAnalytics_GoogleGemini.py
@@ -10,22 +10,14 @@ st.title("Image - analytics using Google Gemini model with Python")
 uploaded_file = st.file_uploader("Upload Image", type =["png","jpg","jpeg"])
 if uploaded_file is not None:
   img = Image.open(uploaded_file)
-  st.image(img,caption="Uploaded Image")
+  resized_image = img.resize((80,80))
+  st.image(resized_image,caption="Uploaded Image",use_container_width=False)
 
 
 prompt = st.text_input("Enter the text ")
 
 if st.button("GET RESPONSE"):
-  img = Image.open(uploaded_file)
-  
-  new_width = 80
-  aspect_ratio = img.height / img.width
-  new_height = int(new_width * aspect_ratio)
-  resized_image = img.resize((new_width, new_height))
-
-   # Display the resized image
-  st.image(resized_image, caption="Resized Image", use_container_width=False)
-    
+  img = Image.open(uploaded_file)    
   model = genai.GenerativeModel("gemini-1.5-flash")
   response = model.generate_content([prompt,img])
   st.markdown(response.text)
